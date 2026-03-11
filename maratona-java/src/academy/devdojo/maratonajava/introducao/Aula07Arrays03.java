e94dc625ebb9f9020d8672a952c633b585dfdeec
lmiguelrs <luizmiguel1002009@hotmail.com>
2026-03-11 15:33:54 -0300

    Docs: Adicionado a segunda parte sobre Arrays

--- maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
+++ maratona-java/src/academy/devdojo/maratonajava/introducao/Aula07Arrays03.java
@@ -9,7 +9,7 @@ public class Aula07Arrays03 {
       //  for (int i = 0; i < numeros3.length ; i++) {
       //      System.out.println(numeros3[i]);
      //   }
-        num = 0;
+
         for (int num : numeros3) {
             System.out.println(num);
         }
