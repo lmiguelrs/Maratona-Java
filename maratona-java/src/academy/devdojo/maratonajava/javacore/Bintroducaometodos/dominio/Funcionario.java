0895e15a99dcb220dc40d95fcb5a7d7b322088b3
lmiguelrs <luizmiguel1002009@hotmail.com>
2026-03-26 16:00:16 -0300

    docs: Adicionado quarta parte sobre Orientação a Objetos e Exercicios

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -1,9 +1,10 @@
 package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void imprime() {
         System.out.println("-----------------");
@@ -22,11 +23,38 @@ public class Funcionario {
         if (salarios == null) {
             return;
         }
-        double media = 0;
         for (double salario : salarios) {
             media += salario;
         }
         media /= salarios.length;
         System.out.println("Média Salarial: " + media);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }
--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -9,17 +9,17 @@ public class FuncionarioTest01 {
         Funcionario funcionario02 = new Funcionario();
         Funcionario funcionario03 = new Funcionario();
 
-        funcionario01.nome = "Miguel";
-        funcionario01.idade = 23;
-        funcionario01.salarios = new double[]{1500, 1000, 1500};
+        funcionario01.setNome("Miguel");
+        funcionario01.setIdade(23);
+        funcionario01.setSalarios(new double[]{1500, 1000, 1500});
 
-        funcionario02.nome = "Luiz";
-        funcionario02.idade = 30;
-        funcionario02.salarios = new double[]{1000, 1000, 1000};
+        funcionario02.setNome("Luiz");
+        funcionario02.setIdade(30);
+        funcionario02.setSalarios(new double[]{1000, 1000, 1000});
 
-        funcionario03.nome = "Jorge";
-        funcionario03.idade = 28;
-        funcionario03.salarios = new double[]{2000, 1500, 1500};
+        funcionario03.setNome("Jorge");
+        funcionario03.setIdade(28);
+        funcionario03.setSalarios(new double[]{2000, 1500, 1500});
 
         funcionario01.imprime();
         funcionario02.imprime();
