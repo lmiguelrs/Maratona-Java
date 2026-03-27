67f463f31382f0646d15eb8c11a204c6f1dd4683
lmiguelrs <luizmiguel1002009@hotmail.com>
2026-03-26 22:33:34 -0300

    docs: adicionado quinta parte sobre sobrecarga de metodos

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/PessoaTest01.java
@@ -7,7 +7,7 @@ public class PessoaTest01 {
         Pessoa pessoa = new Pessoa();
         pessoa.setNome("Jiraya");
         pessoa.setIdade(70);
-      //  pessoa.imprime();
+        //  pessoa.imprime();
         System.out.println(pessoa.getNome());
         System.out.println(pessoa.getIdade());
     }
