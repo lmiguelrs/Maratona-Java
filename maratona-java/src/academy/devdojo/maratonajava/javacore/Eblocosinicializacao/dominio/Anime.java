ede1ea8f21f08c2ce5f7229d589e31d317db2460
lmiguelrs <luizmiguel1002009@hotmail.com>
2026-04-04 23:34:57 -0300

    docs: Adicionado quarta pare sobre Orientação a Objetos focada em Construtores, Blocos de Inicialização e Métodos Estáticos

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Eblocosinicializacao/dominio/Anime.java
@@ -0,0 +1,36 @@
+package academy.devdojo.maratonajava.javacore.Eblocosinicializacao.dominio;
+
+public class Anime {
+    private String nome;
+    private int[] episodios;
+    // 1 - O espaço na memória é alocado para o objeto
+    // 2 - Cada atributo de classe é inicializado com valores defalut ou o que for passado
+    // 3 - Bloco de inicialização é executado
+    // 4 - Construtor é executado
+    {
+        System.out.println("Dentro do bloco de inicialização");
+        episodios = new int[100];
+        for (int i = 0 ; i < episodios.length ; i++) {
+            episodios[i] = i+1;
+        }
+    }
+
+    public Anime(String nome) {
+        this.nome = nome;
+    }
+
+    public Anime() {
+        for(int episodio : this.episodios) {
+            System.out.print(episodio + " ");
+        }
+        System.out.println();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int[] getEpisodios() {
+        return episodios;
+    }
+}
